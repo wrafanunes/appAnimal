3aee7ff27d11b0d835a6927c66ed875d5bd15489
Wilson Rafael <36869347+wrafanunes@users.noreply.github.com>
2019-12-03 09:33:16 -0300

    Modificando a listagem dos animais cadastrados no firebase

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Animal } from '../interfaces/animal';
+import { Subscription } from 'rxjs';
+import { AnimalService } from '../services/animal.service';
+import { NavController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-list',
@@ -6,31 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
-  private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor() {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+  public animais=new Array<Animal>();
+  public animaisSubscription:Subscription;
+  loading: any;
+  loadingCtrl: any;
+  toastCtrl: any;
+  constructor(public animaisService:AnimalService,public authService:AuthService) {
+    this.animaisSubscription=this.animaisService.getAnimais().subscribe(data=>{
+      this.animais=data;
+    });
   }
 
   ngOnInit() {
+  }
+  ngOnDestroy(){
+    this.animaisSubscription.unsubscribe();
+  }
+  async logout(){
+try{
+  await this.authService.logout();
+}catch(error){
+  console.error(error);
+}
+  }
+  async deleteAnimal(id:string){
+    try {
+      await this.animaisService.deleteAnimal(id);
+    } catch (error) {
+      this.presentToast('Erro ao tentar excluir');
+    }
+  }
+  async presentLoading() {
+    this.loading=await this.loadingCtrl.create({message: 'Por favor, aguarde...'});
+    return this.loading.present();
+  }
+  async presentToast(message:string){
+    const toast=await this.toastCtrl.create({message,duration:2000});
+    toast.present();
   }
   // add back when alpha.4 is out
   // navigate(item) {
