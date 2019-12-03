8cf470f8d052e9bfa421493aafdf7676ea004fca
Wilson Rafael <36869347+wrafanunes@users.noreply.github.com>
2019-12-03 09:28:33 -0300

    Criando a interface Animal

---
+++ src/app/interfaces/animal.ts
@@ -0,0 +1,9 @@
+export interface Animal {
+    id?: string;
+    name?: string;
+    description?: string;
+    picture?: string;
+    price?: string;
+    userId?: string;
+    createdAt?: number;
+}
